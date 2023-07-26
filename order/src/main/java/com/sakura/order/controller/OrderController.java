c712ae3dcacde40166c9c8b425b894217a6e4ceb
peixing <px123456>
2023-07-26 14:46:26 +0800

    整合nacos配置中心

--- order/src/main/java/com/sakura/order/controller/OrderController.java
+++ order/src/main/java/com/sakura/order/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.sakura.order.controller;
 import com.sakura.order.feign.ProductFeignService;
 import com.sakura.order.feign.StockFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
+@RefreshScope
 public class OrderController {
 
     @Autowired
@@ -19,12 +22,21 @@ public class OrderController {
     @Autowired
     ProductFeignService productFeignService;
 
+    @Value("${user.name}")
+    private String userName;
+
+    @Value("${user.age}")
+    private String userAge;
+
+    @Value("${user.sex}")
+    private String userSex;
+
     @RequestMapping("/add")
     public String add(){
         System.out.println("下单成功");
         String msg = stockFeignService.reduct();
         String productMsg = productFeignService.get(1);
-        return "下单成功" + msg + "-" + productMsg;
+        return userName + userAge + userSex +  "下单成功" + msg + "-" + productMsg;
     }
 
 }
